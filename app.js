fae945ae6470c5969619eabe4cbf89159acc2a48
Effimowsky <o.effimowsky@gmail.com>
2022-03-03 15:57:37 +0400

    add recomendations content

--- app.js
+++ app.js
@@ -46,3 +46,34 @@ window.addEventListener("scroll", () => {
     hideProgress();
   }
 });
+
+// Recomendations
+{
+  var slideIndex = 1;
+  showSlide(slideIndex);
+
+  function currentSlide(n) {
+    showSlide((slideIndex = n));
+  }
+
+  function showSlide(n) {
+    var i;
+    var slide = document.getElementsByClassName("recomendation-content");
+    var dots = document.getElementsByClassName("recomendation-btn");
+    if (n > slide.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = slide.length;
+    }
+    for (i = 0; i < slide.length; i++) {
+      slide[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slide[slideIndex - 1].style.display = "block";
+    slide[slideIndex - 1].style.display = "flex";
+    dots[slideIndex - 1].className += " active";
+  }
+}
